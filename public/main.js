98caa97d38ca097d8dda889931950554f3c87316
Aman Gupta <amangupta052@gmail.com>
2025-07-08 15:08:21 +0800

    fixup

--- public/main.js
+++ public/main.js
@@ -1,8 +1,26 @@
 (() => {
   // <stdin>
   (function() {
-    document.documentElement.classList.remove("dark");
-    localStorage.theme = "light";
+    const themeToggle = document.querySelector(".darkmode-toggle input");
+    const light = "light";
+    const dark = "dark";
+    let isDark = localStorage.theme === dark || !("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches;
+    if (isDark) {
+      document.documentElement.classList.add(dark);
+      themeToggle.checked = true;
+    } else {
+      document.documentElement.classList.remove(dark);
+      themeToggle.checked = false;
+    }
+    themeToggle.addEventListener("change", function() {
+      if (this.checked) {
+        localStorage.theme = dark;
+        document.documentElement.classList.add(dark);
+      } else {
+        localStorage.theme = light;
+        document.documentElement.classList.remove(dark);
+      }
+    });
     const navbarMenuToggle = document.getElementById("navbar-menu-toggle");
     const navbarMenu = document.getElementById("navbar-menu");
     const navbarLangToggle = document.getElementById("navbar-lang-toggle") || document.createElement("div");
